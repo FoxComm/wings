6b9a51281bc6a5b923800ac5fc1afc6fee939ed7
Andrey Rublev <version.ru@gmail.com>
2016-12-13 17:46:04 +0300

    more robust approch in skuIdentity method

--- src/paragons/sku.js
+++ src/paragons/sku.js
@@ -1,6 +1,8 @@
 // @flow
 
+import _ from 'lodash';
+
 // returns unique in terms of cart/order key for sku line item
 export function skuIdentity(sku: Object): string {
-  return sku.referenceNumbers[0] || sku.sku;
+  return _.get(sku, ['referenceNumbers', 0]) || sku.sku;
 }
